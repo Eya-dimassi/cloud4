d595ad6424410f29b95fc0249e453c60e2b8114d
eyadim <ayadimassi54@gmail.com>
2025-11-26 01:26:43 +0100

    section 10

--- pays/src/main/java/com/eya/pays/restControllers/PaysController.java
+++ pays/src/main/java/com/eya/pays/restControllers/PaysController.java
@@ -1,8 +1,10 @@
 package com.eya.pays.restControllers;
+import com.eya.pays.config.Configuration;
 import com.eya.pays.dto.APIResponseDto;
 import com.eya.pays.dto.PaysDto;
 import com.eya.pays.service.PaysService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +15,8 @@ import org.springframework.web.bind.annotation.*;
 public class PaysController {
 
     private final PaysService paysService;
-
+    @Autowired
+    Configuration configuration;
     @GetMapping("{id}")
     public ResponseEntity<APIResponseDto> getPays(@PathVariable("id") Long id) {
 
@@ -21,5 +24,10 @@ public class PaysController {
                 paysService.getPaysById(id),
                 HttpStatus.OK
         );
+    }
+    @GetMapping("/author")
+    public ResponseEntity<String> retrieveAuthorInfo() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(configuration.getName()+" "+configuration.getEmail() );
     }
 }
